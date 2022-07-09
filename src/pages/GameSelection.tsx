b230018e8e2f2cdf949c73df6b6e485eb73e5103
electropicks <lkbidy@gmail.com>
2022-07-09 02:37:43 +0000

    wrapped game containers in page container

--- src/pages/GameSelection.tsx
+++ src/pages/GameSelection.tsx
@@ -3,23 +3,31 @@ import styled from "styled-components";
 
 const GameSelection = () => {
     return (
-        <div>
-        <GameContainer order={1}></GameContainer>
-        <GameContainer order={2}></GameContainer>
-        <GameContainer order={3}></GameContainer>
-        <GameContainer order={4}></GameContainer>
-        <GameContainer order={5}></GameContainer>
-        <GameContainer order={6}></GameContainer>
-        </div>
+        <PageContainer>
+            
+                <GameContainer order={1}></GameContainer>
+                <GameContainer order={2}></GameContainer>
+                <GameContainer order={3}></GameContainer>
+                <GameContainer order={4}></GameContainer>
+                <GameContainer order={5}></GameContainer>
+                <GameContainer order={6}></GameContainer>
+        </PageContainer>
     )
 }
 
 export default GameSelection;
 
+const PageContainer = styled.div `
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: purple;
+
+`
+
 const GameContainer = styled.div<GameProps>`
     position: absolute;
-    top: ${props => props.order < 4 ? "30%" : "60%"};
-    left: ${props => (props.order > 3 ? props.order - 3 : props.order) * 25 + "%" };
+    top: ${props => props.order <= 3 ? "0": "0"};
     background-color: #eaeaea;
     height: 200px;
     width: 200px;
