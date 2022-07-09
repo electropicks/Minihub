09714ad4e6c24f66b17393a4bfeb1645a81d8116
electropicks <lkbidy@gmail.com>
2022-07-09 02:10:35 +0000

    made container element instead of border

--- src/pages/GameSelection.jsx
+++ src/pages/GameSelection.jsx
@@ -13,5 +13,7 @@ const GameContainer = styled.div`
     position: absolute;
     top: ${props => props.order < 4 ? "30%" : "60%"};
     left: ${props => (props.order > 3 ? props.order - 3 : props.order) * 25 + "%" };
-    border: 20px solid green;
+    background-color: #eaeaea;
+    height: 200px;
+    width: 200px;
 `
